5dc7c4a0f924fe865597822a5e7e57e7597f2719
Sam Perozek <samperozek@gmail.com>
2022-06-08 12:00:53 -0500

    commit tree b91f40af0988d4bf011225160d8ec07a495f70f5

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+
+const dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+  function moveDodgerRight() {
+    const leftNumbers2 = dodger.style.left.replace("px", "");
+    const left2 = parseInt(leftNumbers2, 10);
+  
+    if (left2 > 0) {
+      dodger.style.left = `${left2 + 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
